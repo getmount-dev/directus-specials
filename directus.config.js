d198de3afd6735ad8585465ae8ec024d70dc4a22
Zaritsky Pavel <zaritchiip@gmail.com>
2023-06-20 09:42:20 +0300

    add cors

--- directus.config.js
+++ directus.config.js
@@ -17,11 +17,9 @@ module.exports = function (env) {
 		// CORS
 		CORS_ENABLED: "true",
 		CORS_ORIGIN: [
-			"http://localhost",
-			"http://localhost:*",
 			"http://localhost:3333",
-			"*.specialscomedy.com",
-			"specialscomedy.com",
+			"https://*.specialscomedy.com",
+			"https://specialscomedy.com",
 			"https://cloud.hasura.io",
 		],
 		CORS_METHODS: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
